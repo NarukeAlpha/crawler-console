5381fe9b5b6033e4842e1195e4b13c0637aad036
Gabriel Alfonzo <bagaao@live.com>
2024-08-23 17:41:42 -0400

    quick fix for github actions

---
+++ integrationTest.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	url, isPresent := os.LookupEnv("LS_URL")
+	if !isPresent {
+		url = "127.0.0.1"
+	}
+	var host = "http://" + url + ":8080/v1/healthcheck"
+	for i := 0; i < 10; i++ {
+		req, err := http.Get(host)
+		if err != nil {
+			log.Println("Failed to ping service: %v", err)
+		}
+		defer req.Body.Close()
+		if req.StatusCode == 200 {
+			log.Println("Log Service is healthy")
+			os.Exit(0)
+		}
+		time.Sleep(5 * time.Second)
+	}
+	log.Println("Failed to connect to log service in 30 seconds, closing with error")
+	os.Exit(1)
+}
